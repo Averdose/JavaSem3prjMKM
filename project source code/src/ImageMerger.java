caff8720de2b31cfcd51d4a31322cbc638fa8c94
Kornel <kornelzaba@gmail.com>
2016-01-04 20:13:02 +0100

    Dodałem fading/shading, ogarniecie o co chodzi. Arbajt, arbajt.

--- project source code/src/ImageMerger.java
+++ project source code/src/ImageMerger.java
@@ -1,23 +1,31 @@
 import java.awt.Graphics;
+import java.awt.Color;
 import java.awt.image.*;
 import java.io.*;
 import java.lang.Math;
 import javax.imageio.ImageIO;
 
 public class ImageMerger {	
-	
+	private int minWidth, minHeight;
+	private int maxWidth, maxHeight;
+	private int mode;
+	private BufferedImage baseImageA, baseImageB;
+	public ImageMerger() {}
+	public ImageMerger(int _mode) {
+		mode = _mode;
+	}
 	/*TO IMPLEMENT private functions"
 	 * setWhiteBackground()
 	 * setImageExtension()
 	 * getImageExtension()
 	 * setBitsPerPixel()
 	 */
-	private void mergePixels(BufferedImage baseImageA, BufferedImage baseImageB, BufferedImage newImage, int width, int height, int mode) {
+	private void mergePixels(BufferedImage newImage, int width, int height) {
 		if(baseImageA.getWidth() >= baseImageB.getWidth() && baseImageA.getHeight() <= baseImageB.getHeight()) {
-			int minWidth = (width - baseImageB.getWidth())/2;
-			int maxWidth = minWidth + baseImageB.getWidth();
-			int minHeight = (height - baseImageA.getHeight())/2;
-			int maxHeight = minHeight + baseImageA.getHeight();
+			minWidth = (width - baseImageB.getWidth())/2;
+			maxWidth = minWidth + baseImageB.getWidth();
+			minHeight = (height - baseImageA.getHeight())/2;
+			maxHeight = minHeight + baseImageA.getHeight();
 			
 			for(int w = minWidth, i = 0; w < maxWidth; w++, i++)
 				for(int h = minHeight, j = 0; h < maxHeight; h++, j++) {
@@ -37,10 +45,10 @@ public class ImageMerger {
 				}
 		}
 		else if(baseImageA.getWidth() <= baseImageB.getWidth() && baseImageA.getHeight() >= baseImageB.getHeight()) {
-			int minWidth = (width - baseImageA.getWidth())/2;
-			int maxWidth = minWidth + baseImageA.getWidth();
-			int minHeight = (height - baseImageB.getHeight())/2;
-			int maxHeight = minHeight + baseImageB.getHeight();
+			minWidth = (width - baseImageA.getWidth())/2;
+			maxWidth = minWidth + baseImageA.getWidth();
+			minHeight = (height - baseImageB.getHeight())/2;
+			maxHeight = minHeight + baseImageB.getHeight();
 			
 			for(int w = minWidth, i = 0; w < maxWidth; w++, i++)
 				for(int h = minHeight, j = 0; h < maxHeight; h++, j++) {
@@ -59,9 +67,10 @@ public class ImageMerger {
 				}
 		}
 	}
-	public BufferedImage mergeImages(String pathA, String pathB, int mode) throws IOException{
-		BufferedImage baseImageA = ImageIO.read(new File(pathA));
-		BufferedImage baseImageB = ImageIO.read(new File(pathB));
+	
+	public BufferedImage mergeImages(String pathA, String pathB) throws IOException{
+		baseImageA = ImageIO.read(new File(pathA));
+		baseImageB = ImageIO.read(new File(pathB));
 		
 		int width = Math.max(baseImageA.getWidth(), baseImageB.getWidth());
 		int height = Math.max(baseImageA.getHeight(), baseImageB.getHeight());
@@ -72,7 +81,7 @@ public class ImageMerger {
 		graph.drawImage(baseImageA, (width - baseImageA.getWidth())/2, (height - baseImageA.getHeight())/2, null);
 		graph.drawImage(baseImageB, (width - baseImageB.getWidth())/2, (height - baseImageB.getHeight())/2, null);
 		
-		mergePixels(baseImageA, baseImageB, newImage, width, height, mode);
+		mergePixels(newImage, width, height);
 		/*only for tests*/
 		ImageIO.write(newImage, "BMP", new File("newImage.bmp"));
 		
@@ -81,5 +90,112 @@ public class ImageMerger {
 		baseImageB.flush();
 		
 		return newImage;
+	}
+	
+	/*direction: 0 - from L to R, 1 - from TL to BR, 2 - from T to B, 3 - from TR to BL
+	 * 			 4 - from R to L, 5 - from BR to TL, 6 - from B to T, 7 - from BL to TR
+	 */
+	private int getGradient(int direction, int currentWidth, int currentHeight){
+		switch(direction) {
+		case 0 :
+			float ratio = (float) currentWidth / maxWidth;
+			return (int) (ratio * 255);
+		case 1 :
+			int area = maxWidth * maxHeight;
+			int currentArea = currentWidth * currentHeight;
+			ratio = (float) currentArea / area;
+			return (int) (ratio * 255);
+		case 2 :
+			ratio = (float) currentHeight / maxHeight;
+			return (int) (ratio * 255);
+		case 3 :
+			area = maxWidth * maxHeight;
+			currentArea = (maxWidth - (currentWidth - minWidth)) * currentHeight;
+			ratio = (float) currentArea / area;
+			return (int) (ratio * 255);
+		case 4 :
+			int width = maxWidth - (currentWidth - minWidth);
+			ratio = (float) width / maxWidth;
+			return (int) (ratio * 255);
+		case 5 :
+			area = maxWidth * maxHeight;
+			currentArea = (maxWidth - (currentWidth - minWidth)) * (maxHeight - (currentHeight - minHeight));
+			ratio = (float) currentArea / area;
+			return (int) (ratio * 255);
+		case 6 :
+			int height = maxHeight - (currentHeight - minHeight);
+			ratio = (float) height / maxHeight;
+			return (int) (ratio * 255);
+		case 7 :
+			area = maxWidth * maxHeight;
+			currentArea = currentWidth * (maxHeight - (currentHeight - minHeight));
+			ratio = (float) currentArea / area;
+			return (int) (ratio * 255);
+		}
+		return -1;
+	}
+	
+	public BufferedImage shadeImage(BufferedImage image, int direction) throws IOException {
+		if(direction == 2 || direction == 6) {
+			for(int h = minHeight; h < maxHeight; h++) {
+				for(int w = minWidth; w < maxWidth; w++) {
+					int pixel = image.getRGB(w, h);
+					int gradient = getGradient(direction, w ,h);
+					Color baseColor = new Color(pixel);
+					Color newColor = new Color(Math.max(baseColor.getRed() - gradient, 0),
+							Math.max(baseColor.getGreen() - gradient, 0),
+							Math.max(baseColor.getBlue() - gradient, 0));
+					image.setRGB(w,  h, newColor.getRGB());
+				}
+			}
+		}
+		else {
+			for(int w = minWidth; w < maxWidth; w++) {
+				for(int h = minHeight; h < maxHeight; h++) {
+					int pixel = image.getRGB(w, h);
+					int gradient = getGradient(direction, w ,h);
+					Color baseColor = new Color(pixel);
+					Color newColor = new Color(Math.max(baseColor.getRed() - gradient, 0),
+							Math.max(baseColor.getGreen() - gradient, 0),
+							Math.max(baseColor.getBlue() - gradient, 0));
+					image.setRGB(w,  h, newColor.getRGB());	
+				}
+			}
+		}
+		// only for tests //
+		ImageIO.write(image, "BMP", new File("shadeImage.bmp"));
+		return image;
+	}
+	
+	public BufferedImage fadeImage(BufferedImage image, int direction) throws IOException {
+		if(direction == 2 || direction == 6) {
+			for(int h = minHeight; h < maxHeight; h++) {
+				for(int w = minWidth; w < maxWidth; w++) {
+					int pixel = image.getRGB(w, h);
+					int gradient = getGradient(direction, w ,h);
+					Color baseColor = new Color(pixel);
+					Color newColor = new Color(Math.min(baseColor.getRed() + gradient, 254),
+							Math.min(baseColor.getGreen() + gradient, 254),
+							Math.min(baseColor.getBlue() + gradient, 254));
+					image.setRGB(w,  h, newColor.getRGB());
+				}
+			}
+		}
+		else {
+			for(int w = minWidth; w < maxWidth; w++) {
+				for(int h = minHeight; h < maxHeight; h++) {
+					int pixel = image.getRGB(w, h);
+					int gradient = getGradient(direction, w ,h);
+					Color baseColor = new Color(pixel);
+					Color newColor = new Color(Math.min(baseColor.getRed() + gradient, 254),
+							Math.min(baseColor.getGreen() + gradient, 254),
+							Math.min(baseColor.getBlue() + gradient, 254));
+					image.setRGB(w,  h, newColor.getRGB());	
+				}
+			}
+		}
+		// only for tests //
+		ImageIO.write(image, "BMP", new File("fadeImage.bmp"));
+		return image;
 	}
 }
--- project source code/src/Main.java
+++ project source code/src/Main.java
@@ -1,14 +1,19 @@
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 public class Main {
 	public static void main(String[] args) {
-		String path1 = null, path2 = null;
+		String path1 = "C:\\java images\\clefG\\41.bmp",
+				path2 = "C:\\Users\\Kornel\\Desktop\\54.bmp";
 		
-		ImageMerger imageMerger = new ImageMerger();
+		ImageMerger imageMerger = new ImageMerger(0);
 		
 		try {
 			/*XOR==0, OR==1, AND==2*/
-			imageMerger.mergeImages(path1, path2, 0).flush();
+			BufferedImage img =	imageMerger.mergeImages(path1, path2);
+			imageMerger.fadeImage(img, 7);
+			imageMerger.shadeImage(img, 7);
+			img.flush();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
