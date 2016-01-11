e8148c4a502931d26330c18effbe9a0f72be72e4
Kornel <kornelzaba@gmail.com>
2016-01-11 21:52:30 +0100

    ajn folk, ajn raih, ajn branch

---
+++ project source code/src/MainFrame.java
@@ -0,0 +1,152 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JToolBar;
+import java.awt.GridLayout;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+	static final String[] EXTENSIONS = new String[]{
+	        "gif", "png", "bmp","jpeg","JPEG","jpg" // and other formats you need
+	    };
+
+	static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+
+        @Override
+        public boolean accept(final File dir, final String name) {
+            for (final String ext : EXTENSIONS) {
+                if (name.endsWith("." + ext)) {
+                    return (true);
+                }
+            }
+            return (false);
+        }
+    };
+	private JToolBar toolBar;
+	private JTextField textField;
+	private JTextField textField_1;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 812, 504);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("New button");
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				{
+					JFileChooser fileChooser = new JFileChooser();
+					fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+					fileChooser.setAcceptAllFileFilterUsed(false);
+				    
+					int i =0;
+					
+					if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					File dir = fileChooser.getSelectedFile();
+					textField.setText(dir.getAbsolutePath());
+					 if (dir.isDirectory()) {
+					for (final File f : dir.listFiles(IMAGE_FILTER)) {
+						System.out.println(f.getAbsolutePath()+i);
+						i++;
+						JButton button = new JButton();
+						button.setIcon(new ImageIcon(f.getAbsolutePath()));
+						toolBar.add(button);	
+					}
+					 }
+				    }
+					
+					
+					
+					
+					
+					
+					
+				}
+			
+			
+			
+			}
+		});
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JPanel panel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 23, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(317)
+							.addComponent(btnNewButton))
+						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 532, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnNewButton)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 385, GroupLayout.PREFERRED_SIZE)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 393, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		panel.setLayout(new GridLayout(20, 1, 0, 0));
+		
+		textField_1 = new JTextField();
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		toolBar = new JToolBar();
+		scrollPane.setViewportView(toolBar);
+		contentPane.setLayout(gl_contentPane);
+	}
+}
