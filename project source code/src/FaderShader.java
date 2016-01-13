771bbe391b54525caade926f410216e8f10c0036
Kornel <kornelzaba@gmail.com>
2016-01-13 21:34:41 +0100

    Suuabo

--- project source code/src/FaderShader.java
+++ project source code/src/FaderShader.java
@@ -13,41 +13,57 @@ public class FaderShader {
 		blackOnWhite = _blackOnWhite;
 		direction = _direction;
 	}
-	private int getGradient(int currentWidth, int currentHeight){
+	private int getGradient(int currentWidth, int currentHeight, boolean grayShade){
 		switch(direction) {
 		case 0 :
 			float ratio = (float) currentWidth / maxWidth;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 1 :
 			int area = maxWidth * maxHeight;
 			int currentArea = currentWidth * currentHeight;
 			ratio = (float) currentArea / area;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 2 :
 			ratio = (float) currentHeight / maxHeight;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 3 :
 			area = maxWidth * maxHeight;
 			currentArea = (maxWidth - currentWidth) * currentHeight;
 			ratio = (float) currentArea / area;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 4 :
 			int width = maxWidth - currentWidth;
 			ratio = (float) width / maxWidth;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 5 :
 			area = maxWidth * maxHeight;
 			currentArea = (maxWidth - currentWidth) * (maxHeight - currentHeight);
 			ratio = (float) currentArea / area;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 6 :
 			int height = maxHeight - currentHeight;
 			ratio = (float) height / maxHeight;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		case 7 :
 			area = maxWidth * maxHeight;
 			currentArea = currentWidth * (maxHeight - currentHeight); //0 = minHeight
 			ratio = (float) currentArea / area;
+			if(grayShade)
+				return (int) (ratio * 200);
 			return (int) (ratio * 255);
 		}
 		return -1;
@@ -59,20 +75,19 @@ public class FaderShader {
 		for(int h = 0; h < maxHeight; h++) {
 			for(int w = 0; w < maxWidth; w++) {
 				int pixel = image.getRGB(w, h);
-				int gradient = getGradient(w ,h);
 				Color baseColor = new Color(pixel);
 				Color newColor = null;
-				if(blackOnWhite) {
-					float g = (float) gradient / 255;
-					gradient = (int) g * 200;
-					newColor = new Color(Math.min(baseColor.getRed() + gradient, 200),
-							Math.min(baseColor.getGreen() + gradient, 200),
-							Math.min(baseColor.getBlue() + gradient, 200));
+				if(!blackOnWhite) {
+					int gradient = getGradient(w , h, false);
+					newColor = new Color(Math.min(baseColor.getRed() + gradient, 254),
+							Math.min(baseColor.getGreen() + gradient, 254),
+							Math.min(baseColor.getBlue() + gradient, 254));
 				}
 				else {
-					newColor = new Color(Math.max(baseColor.getRed() - gradient, 0),
-							Math.max(baseColor.getGreen() - gradient, 0),
-							Math.max(baseColor.getBlue() - gradient, 0));
+					int gradient = getGradient(w , h, true);
+					newColor = new Color(Math.max(baseColor.getRed() - gradient, 55),
+							Math.max(baseColor.getGreen() - gradient, 55),
+							Math.max(baseColor.getBlue() - gradient, 55));
 				}
 				image.setRGB(w,  h, newColor.getRGB());
 			}
@@ -88,17 +103,16 @@ public class FaderShader {
 		for(int h = 0; h < maxHeight; h++) {
 			for(int w = 0; w < maxWidth; w++) {
 				int pixel = image.getRGB(w, h);
-				int gradient = getGradient(w ,h);
 				Color baseColor = new Color(pixel);
 				Color newColor = null;
 				if(blackOnWhite) {
+					int gradient = getGradient(w , h, false);
 					newColor = new Color(Math.min(baseColor.getRed() + gradient, 254),
 							Math.min(baseColor.getGreen() + gradient, 254),
 							Math.min(baseColor.getBlue() + gradient, 254));
 				}
 				else {
-					float g = (float) gradient / 255;
-					gradient = (int) g * 200;
+					int gradient = getGradient(w , h, true);
 					newColor = new Color(Math.max(baseColor.getRed() - gradient, 55),
 							Math.max(baseColor.getGreen() - gradient, 55),
 							Math.max(baseColor.getBlue() - gradient, 55));
