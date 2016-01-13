9251619d9ea6cd5abf72dd73e1d48f5ac68deef3
Mikołaj <Mikolaj.Slon@wp.pl>
2016-01-13 17:44:56 +0100

    a lot of stuff

--- project source code/src/MainFrame.java
+++ project source code/src/MainFrame.java
@@ -3,9 +3,11 @@ import java.awt.EventQueue;
 import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.imageio.ImageIO;
+import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
@@ -25,9 +27,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JToolBar;
 import java.awt.GridLayout;
 import java.awt.RenderingHints;
+import javax.swing.JRadioButton;
 
 public class MainFrame extends JFrame {
 
+	private int mergeMode =0;
+	private List<String> listToMerge = new ArrayList<String>();
 	private final List<JButton> listImgs = new ArrayList<JButton>();
 	private final List<JTextField> listDirs = new ArrayList<JTextField>();
 	private JPanel contentPane;
@@ -86,6 +91,8 @@ public class MainFrame extends JFrame {
 		scrollPane_1.setViewportView(toolBar_1);
 		panel.setLayout(new GridLayout(20, 1, 0, 0));
 		
+		final JPanel panel_1 = new JPanel();
+		
 		JButton refresh = new JButton("fuck the refresh butt");
 		refresh.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -129,45 +136,39 @@ public class MainFrame extends JFrame {
 									
 									Integer in = (Integer)button.getClientProperty("selected");
 									
-									if(button.getToolTipText() != "")
+									if(in ==0)
 									{
 										toolBar_1.add(button);
-										button.setToolTipText("");
+										listToMerge.add(button.getToolTipText());
+										button.putClientProperty("selected",new Integer(1));
+										
 										
 									}
 									else
 									{
 										toolBar.add(button);
-										button.setText("");
+										for ( int i = 0;  i < listToMerge.size(); i++){
+								            String tempName = listToMerge.get(i);
+								            if(tempName.equals(button.getToolTipText())){
+								                listToMerge.remove(i);
+								            }
+								        }
+										button.putClientProperty("selected", new Integer(0));
 									}
-									//	button.putClientProperty("selected", new Integer(1));
-									//System.out.println(in);
 									
 									
 									
 									revalidate();
 									repaint();
-									/*
-									ActionListener[] actions = button.getActionListeners();
-									for(ActionListener a : actions)
-										button.removeActionListener(a);
 									
-									button.addActionListener(new ActionListener() {
-										public void actionPerformed(ActionEvent arg0) {
-											toolBar.add(button);
-											
-											revalidate();
-											repaint();
-										}
-										});*/
 								}
 							});
 							resized.flush();
 							img.flush();
 							
 							
-							//toolBar.add(button);	
-							//button.putClientProperty("selected", new Integer(0));
+							toolBar.add(button);	
+							button.putClientProperty("selected", new Integer(0));
 						
 						}
 					}
@@ -181,6 +182,7 @@ public class MainFrame extends JFrame {
 				toolBar.removeAll();
 				toolBar_1.removeAll();
 				listImgs.clear();
+				listToMerge.clear();
 				j=0;
 				for(int i =0;i<listDirs.size();i++)
 				{
@@ -192,6 +194,94 @@ public class MainFrame extends JFrame {
 			}
 		});
 		
+		JButton btnMergeThisShit = new JButton("Merge this Shit");
+		btnMergeThisShit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ImageMerger imgMerger1 = new ImageMerger(mergeMode);
+				BufferedImage img = null;
+				//for(int i =0; i<listToMerge.size();i++)
+				//	System.out.println(listToMerge.get(i));
+				img = imgMerger1.Merge(listToMerge);
+				
+				/*BufferedImage resized = new BufferedImage(200, 200, img.getType());
+				Graphics2D g = resized.createGraphics();
+				g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+				g.drawImage(img, 0, 0, 200, 200, 0, 0, img.getWidth(),img.getHeight(), null);
+				g.dispose();
+				*/
+				
+				JLabel picLabel = new JLabel(new ImageIcon(img));
+				panel_1.removeAll();
+				panel_1.add(picLabel);
+				revalidate();
+				repaint();
+			}
+		});
+		
+		JButton btnQuickmergeThisShit = new JButton("QuickMerge this shit");
+		btnQuickmergeThisShit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ImageMerger imgMerge = new ImageMerger(mergeMode);
+				List<String> listStrings = new ArrayList<String>();
+				for(int i =0; i< listDirs.size();i++)
+					listStrings.add(listDirs.get(i).getText());
+				
+				List<BufferedImage> img = new ArrayList<BufferedImage>();
+				img = imgMerge.DirectoryMerge(listStrings);
+				
+				for(int i =0; i<img.size();i++)
+				{
+					BufferedImage resized = new BufferedImage(200, 200, img.get(i).getType());
+					Graphics2D g = resized.createGraphics();
+					g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+					g.drawImage(img.get(i), 0, 0, 200, 200, 0, 0, img.get(i).getWidth(),img.get(i).getHeight(), null);
+					g.dispose();
+					JLabel picLabel = new JLabel(new ImageIcon(resized));
+					panel_1.add(picLabel);
+					
+				}
+				revalidate();
+				repaint();
+			}
+		});
+		
+		JRadioButton rdbtnOr = new JRadioButton("or");
+		rdbtnOr.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				mergeMode = 1;
+			}
+		});
+		
+		JRadioButton rdbtnXor = new JRadioButton("xor");
+		rdbtnXor.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				mergeMode = 0;
+			}
+		});
+		JRadioButton rdbtnAnd = new JRadioButton("and");
+		rdbtnAnd.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				mergeMode = 2;
+			}
+		});
+		  ButtonGroup group = new ButtonGroup();
+		    group.add(rdbtnOr);
+		    group.add(rdbtnXor);
+		    group.add(rdbtnAnd);
+		
+		JRadioButton rdbtnFading = new JRadioButton("fading");
+		
+		JRadioButton rdbtnShading = new JRadioButton("shading");
+		
+		JRadioButton rdbtnNormal = new JRadioButton("normal");
+		
+		JRadioButton rdbtnBlackOnWhite = new JRadioButton("black on white");
+		
+		JRadioButton rdbtnWhiteOnBlack = new JRadioButton("white on black");
+		    
+		    
+
+		
 	
 		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
@@ -201,9 +291,33 @@ public class MainFrame extends JFrame {
 					.addContainerGap()
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(panel, GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(18)
+											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+												.addComponent(rdbtnNormal)
+												.addComponent(rdbtnShading)
+												.addComponent(rdbtnFading)
+												.addComponent(rdbtnXor)
+												.addComponent(rdbtnAnd)
+												.addComponent(rdbtnOr)))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(rdbtnBlackOnWhite))
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(rdbtnWhiteOnBlack))))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(btnQuickmergeThisShit)
+									.addGap(18)
+									.addComponent(btnMergeThisShit)
+									.addGap(15)))
+							.addGap(26)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 								.addGroup(gl_contentPane.createSequentialGroup()
 									.addComponent(btnStarFromThe)
 									.addGap(18)
@@ -211,12 +325,9 @@ public class MainFrame extends JFrame {
 									.addGap(83)
 									.addComponent(btnNewButton)
 									.addGap(100))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 532, GroupLayout.PREFERRED_SIZE)
-									.addContainerGap())))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 777, GroupLayout.PREFERRED_SIZE)
-							.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 615, GroupLayout.PREFERRED_SIZE)))
+						.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 777, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -225,12 +336,40 @@ public class MainFrame extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnNewButton)
 						.addComponent(refresh)
-						.addComponent(btnStarFromThe))
+						.addComponent(btnStarFromThe)
+						.addComponent(btnMergeThisShit)
+						.addComponent(btnQuickmergeThisShit))
 					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(scrollPane)
-						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 385, Short.MAX_VALUE))
-					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 385, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(30)
+									.addComponent(rdbtnAnd)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(rdbtnOr)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnXor)
+									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(rdbtnFading))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 163, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)))
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addGap(18)
+									.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(rdbtnShading)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnNormal)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(rdbtnBlackOnWhite)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(rdbtnWhiteOnBlack)))))
+					.addGap(15)
 					.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
@@ -285,11 +424,37 @@ public class MainFrame extends JFrame {
 							
 							button.addActionListener(new ActionListener() {
 								public void actionPerformed(ActionEvent arg0) {
-									JButton selectedButton = new JButton();
-									selectedButton = button;
-									toolBar_1.add(button);
+									
+									//JButton selectedButton = new JButton(button);
+									
+									Integer in = (Integer)button.getClientProperty("selected");
+									
+									if(in ==0)
+									{
+										toolBar_1.add(button);
+										listToMerge.add(button.getToolTipText());
+										button.putClientProperty("selected",new Integer(1));
+										
+										
+									}
+									else
+									{
+										toolBar.add(button);
+										for ( int i = 0;  i < listToMerge.size(); i++){
+								            String tempName = listToMerge.get(i);
+								            if(tempName.equals(button.getToolTipText())){
+								                listToMerge.remove(i);
+								            }
+								        }
+
+										button.putClientProperty("selected", new Integer(0));
+									}
+									
+									
+									
 									revalidate();
 									repaint();
+									
 								}
 							});
 							
@@ -300,6 +465,7 @@ public class MainFrame extends JFrame {
 							img.flush();
 							
 							toolBar.add(button);	
+							button.putClientProperty("selected", new Integer(0));
 						}
 					}
 			   }
